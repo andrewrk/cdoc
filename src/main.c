79de55b4c3503b81ceee0a56f7f2cb663505fd49
Andrew Kelley <superjoe30@gmail.com>
2015-08-25 23:38:40 -0700

    hello world

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2015 Andrew Kelley
+ *
+ * This file is part of cdoc, which is MIT licensed.
+ * See http://opensource.org/licenses/MIT
+ */
+
+#include "config.h"
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    fprintf(stderr, "works\n");
+}
